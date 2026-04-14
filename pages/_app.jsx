4eabfd29cc96adc7b28581b387bf7625e2024578
Board <board@cloudach.com>
2026-04-14 02:25:57 -0400

    fix(favicon): square viewBox, global favicon in _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
 import '../styles/dashboard.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
